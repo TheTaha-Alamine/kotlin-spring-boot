7434d6ef627bd827beeb9746ad482dfbc87fe9e8
taha boukhalf <tahaboukhalf@gmail.com>
2021-07-16 15:50:34 +0100

    initial commit

---
+++ src/main/kotlin/com/example/inversionOf/InversionOfApplication.kt
@@ -0,0 +1,11 @@
+package com.example.inversionOf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InversionOfApplication
+
+fun main(args: Array<String>) {
+	runApplication<InversionOfApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/inversionOf/InversionOfApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.inversionOf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InversionOfApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
